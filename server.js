4f193865e2df925d57daa327c3eaf32702eac51d
muhammad aqil <muhammad.aqil-sys@vendor.tnpak.com.pk>
2025-02-27 12:36:34 +0500

    replica App added in server

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const path=require("path")
 
 const app=express();
 
-
+const replicaApp=process.env.APP_NAME
 const port=3000
 
 app.use("/images",express.static(path.join(__dirname,'images')))
 
 app.use("/",(req,res)=>{
     res.sendFile(path.join(__dirname,'index.html'))
+    console.log(`request served by ${replicaApp}`)
 })
 
 app.listen(port,()=>{
-    console.log(`running on port ${port}`)
+    console.log(`${replicaApp} running on port ${port}`)
 })
